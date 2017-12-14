40bf46a346aff115048b3ebb9262c7a7f9f74bee
Kishori Bihade <kishori.bihade@gmail.com>
2017-12-14 17:28:28 +0530

    scrool

--- www/js/app.js
+++ www/js/app.js
@@ -33,15 +33,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'AppCtrl'
       })
 
-      .state('app.search', {
-        url: '/search',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/search.html'
-          }
-        }
-      })
-
       .state('player', {
         url: '/player',
         templateUrl: 'templates/player.html',
@@ -59,33 +50,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         url: '/winner',
         templateUrl: 'templates/winner.html',
         controller: 'WinnerCtrl'
-      })
-      .state('app.browse', {
-        url: '/browse',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/browse.html'
-          }
-        }
-      })
-      .state('app.playlists', {
-        url: '/playlists',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlists.html',
-            controller: 'PlaylistsCtrl'
-          }
-        }
-      })
-
-      .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlist.html',
-            controller: 'PlaylistCtrl'
-          }
-        }
       });
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/player');
--- www/js/service.js
+++ www/js/service.js
@@ -155,3 +155,136 @@ myApp.directive('joker', function () {
     }
   };
 })
+var i = 0;
+myApp.directive('animatedCard', function ($ionicGesture, $timeout) {
+  return {
+    restrict: 'E',
+    replace: false,
+    scope: true,
+    templateUrl: 'templates/directive/animatedCard.html',
+    link: function ($scope, $element, $attr) {
+      var distanceDifference = 0;
+      var event = {
+        gesture: {
+          distance: 0
+        }
+      };
+      $scope.card = $attr.card;
+      var cardHeight = 300;
+      var topMargin = 0;
+      var maxDragPercent = 60;
+      $timeout(function () {
+        var cardImage = $($element).find("card.animatedCard img").get(0);
+        var cardImageOpen = $($element).find("card.animatedCardOpen img").get(0);
+
+        var parentImage = $($element).find("card.animatedCard").get(0);
+        var parentImageOpen = $($element).find("card.animatedCardOpen").get(0);
+        cardImage.addEventListener('touchstart', function (e) {
+          distanceStart = e.changedTouches[0].clientY;
+        }, false);
+
+        cardImage.addEventListener('touchmove', function (e) {
+          distanceDifference = distanceStart - e.changedTouches[0].clientY;
+          event.gesture.distance = distanceDifference;
+          var upDistance = event.gesture.distance * -1;
+          var amountUp = (cardHeight - upDistance);
+          if (upDistance >= 0) {
+            var dragPercent = upDistance / cardHeight * 100;
+            if (dragPercent < maxDragPercent) {
+              var topPosition = (cardHeight - (1 * upDistance));
+              $(parentImage).css("height", amountUp + "px");
+              $(parentImage).css("top", upDistance + "px");
+              $(parentImageOpen).css("height", upDistance + "px");
+              $(parentImageOpen).css("top", (topPosition + topMargin) + "px");
+              $scope.$apply();
+            }
+          }
+
+        }, false);
+        cardImage.addEventListener('touchend', function (e) {
+          event.gesture.distance = 0;
+          $(parentImage).css("height", cardHeight + "px");
+          $(parentImageOpen).css("height", "0px");
+          $(parentImage).css("top", 0 + "px");
+          $(parentImageOpen).css("top", cardHeight + topMargin + "px");
+          $scope.$apply();
+        }, false);
+      }, 200);
+
+
+    }
+  };
+
+});
+
+myApp.directive('animatedCardStack', function ($ionicGesture) {
+  return {
+    restrict: 'E',
+    replace: false,
+    scope: {
+      player: "="
+    },
+    templateUrl: 'templates/directive/animatedCardStack.html',
+    link: function ($scope, $element, attr) {
+      $scope.card = attr.card;
+      var cardHeight = 300;
+      var topMargin = 0;
+      var maxDragPercent = 60;
+      $scope.player.dragCss = {
+        width: "100%",
+        overflow: "hidden",
+        top: "0px"
+      };
+      $scope.player.dragCssOpen = {
+        width: "100%",
+        overflow: "hidden",
+        height: "0px"
+      };
+      this.onDrag = function (event) {
+        if (!$scope.player.dragCss) {
+          $scope.player.dragCss = {
+            width: "100%",
+            overflow: "hidden",
+          };
+          $scope.player.dragCssOpen = {
+            width: "100%",
+            overflow: "hidden",
+            height: "0px"
+          };
+        }
+        var upDistance = event.gesture.distance;
+        var amountUp = (cardHeight - upDistance);
+        var dragPercent = upDistance / cardHeight * 100;
+        if (dragPercent < maxDragPercent) {
+          var topPosition = (cardHeight - (1 * upDistance));
+          $scope.player.dragCss.height = amountUp + "px";
+          $scope.player.dragCss.top = upDistance + "px";
+          $scope.player.dragCssOpen.height = upDistance + "px";
+          $scope.player.dragCssOpen.top = (topPosition + topMargin) + "px";
+          $scope.$apply();
+        }
+      };
+      this.onDragEnd = function (event) {
+        if (!$scope.player.dragCss) {
+          $scope.player.dragCss = {
+            width: "100%",
+            overflow: "hidden",
+          };
+          $scope.player.dragCssOpen = {
+            width: "100%",
+            overflow: "hidden",
+            height: "0px"
+          };
+        }
+        $scope.player.dragCss.height = cardHeight + "px";
+        $scope.player.dragCss.top = "0px";
+        $scope.player.dragCssOpen.height = "0px";
+        $scope.player.dragCssOpen.top = cardHeight + topMargin + "px";
+        $scope.$apply();
+      };
+      $ionicGesture.on('dragdown', this.onDrag, $element);
+      $ionicGesture.on('dragend', this.onDragEnd, $element);
+
+    }
+  };
+});
